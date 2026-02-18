744cae479becfd13061aec82636e7e52ad2912d4
Andrew Dupre <andrewdupre@Andrews-MacBook-Pro.local>
2026-02-18 20:18:19 +0400

    feature: Updated 3d model splash

--- client/components/3d-camera/Camera.tsx
+++ client/components/3d-camera/Camera.tsx
@@ -1,16 +1,8 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Spline from "@splinetool/react-spline";
-import Image from "next/image";
 
 const Camera = ({ onAnimationEnd }: { onAnimationEnd: () => void }) => {
-  const [mounted, setMounted] = useState(false);
-
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => setMounted(true), 700);
-
-  //   return () => clearTimeout(timeout);
-  // });
 
   useEffect(() => {
     const timeout = setTimeout(() => onAnimationEnd(), 5500);
@@ -20,18 +12,7 @@ const Camera = ({ onAnimationEnd }: { onAnimationEnd: () => void }) => {
 
   return (
     <div className="h-screen max-h-screen bg-black">
-      {/* {!mounted && (
-        <div className="h-screen max-h-screen grid items-center justify-center">
-          <Image
-            src={"/images/Untitled - Copy@1-1800x919.png"}
-            alt="Camera"
-            height={1500}
-            width={1500}
-          />
-        </div>
-      )} */}
-
-      <Spline scene="https://prod.spline.design/D2ivnVqf9GLJM2h8/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/9p2acX-LHcSO2XFo/scene.splinecode" />
     </div>
   );
 };
